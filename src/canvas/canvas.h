4f479bda0d7687f5886d2a7ee3a8d6b11e7c4b65
Joao Francisco Perpectuo Figueiredo <jperpect@c1r12s3.42porto.com>
2025-03-17 16:20:26 +0000

    chage name the s_color and start canva

--- src/canvas/canvas.h
+++ src/canvas/canvas.h
@@ -6,31 +6,38 @@
 /*   By: jperpct <jperpect@student.42porto.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 15:33:28 by jperpct           #+#    #+#             */
-/*   Updated: 2025/03/17 16:03:26 by jperpct          ###   ########.fr       */
+/*   Updated: 2025/03/17 16:19:52 by jperpct          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CANVAS_H
 # define CANVAS_H
 
-typedef union t_color
+typedef union s_color
 {
 	double red;
 	double green;
 	double blue;
 	int color[3];
-} s_color; 
+} t_color; 
 
-s_color color_new(double red, double green, double blue);
+t_color color_new(double red, double green, double blue);
 
-s_color colors_adding(s_color c1,s_color c2);
+t_color colors_adding(t_color c1,t_color c2);
 
-s_color colors_subtracting(s_color c1,s_color c2);
+t_color colors_subtracting(t_color c1,t_color c2);
 
-s_color colors_scalar_multipl(s_color c1,double c2);
+t_color colors_scalar_multipl(t_color c1,double c2);
 
-s_color colors_multipl(s_color c1,s_color c2);
+t_color colors_multipl(t_color c1,t_color c2);
 
 # define hadamard_product(c1,c2) colors_multipl(c1,c2);
+
+//------------------------------------------------------//
+
+void canva_inicializ(int x, int y, t_color base);
+
+
+
 
 #endif
