512c3c4659ec33bd8cfdb2615bb74658b3a7e963
Joao Francisco Perpectuo Figueiredo <jperpect@c2r6s2.42porto.com>
2025-03-18 14:10:34 +0000

    not leaks error

--- src/canvas/canvas.c
+++ src/canvas/canvas.c
@@ -6,7 +6,7 @@
 /*   By: jperpct <jperpect@student.42porto.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 16:04:04 by jperpct           #+#    #+#             */
-/*   Updated: 2025/03/18 14:00:43 by jperpct          ###   ########.fr       */
+/*   Updated: 2025/03/18 14:09:54 by jperpct          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,6 @@ int canva_loop_key(int keycode,void *param)
 
 	rt_struct = (t_minirt*) param;
 	printf("the key is %d \n",keycode);
-
-
 	return 1;
 }
 
@@ -37,17 +35,15 @@ int canva_loop_mouse(int button,int x,int y,void *param)
 
 void canva_inicializ(t_minirt *rt_struct,int x, int y, t_color base)
 {
-	ft_pocket_new("canva");
 	rt_struct->canva.mlx =ft_add_memory(mlx_init(),NULL);
 	rt_struct->canva.mlx_wind  = mlx_new_window(rt_struct->canva.mlx, x, y, "new minRT");
-//	rt_struct->canva.canva = ft_add_memory(mlx_new_image(rt_struct->canva.mlx, x, y), NULL);
-	ft_pocket_set("main");
+	rt_struct->canva.canva = mlx_new_image(rt_struct->canva.mlx, x, y);
 	mlx_key_hook(rt_struct->canva.mlx_wind, canva_loop_key, rt_struct);
 	mlx_mouse_hook(rt_struct->canva.mlx_wind, canva_loop_mouse, rt_struct);
 	mlx_loop_hook(rt_struct->canva.mlx, canva_loop, rt_struct);
 	mlx_hook(rt_struct->canva.mlx_wind, 17, 01, &mlx_loop_end,rt_struct->canva.mlx);
 	mlx_loop(rt_struct->canva.mlx);
-
+	mlx_destroy_image(rt_struct->canva.mlx,rt_struct->canva.canva);
 	mlx_destroy_window(rt_struct->canva.mlx, rt_struct->canva.mlx_wind);
 	mlx_destroy_display(rt_struct->canva.mlx);
 	ft_free_all();
