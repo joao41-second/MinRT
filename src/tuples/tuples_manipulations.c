b4d121bb0cfc5d0069c4230cfd81dcb7c68f5a9b
reinatch <reinatchnatch@gmail.com>
2025-04-04 20:30:42 +0100

    added ft_printf to test makefile, fix tuples pointers

--- src/tuples/tuples_manipulations.c
+++ src/tuples/tuples_manipulations.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   tuples copy 3.c                                    :+:      :+:    :+:   */
+/*   tuples_manipulations.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rerodrig <rerodrig@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 10:07:37 by rerodrig          #+#    #+#             */
-/*   Updated: 2025/03/24 11:09:52 by rerodrig         ###   ########.fr       */
+/*   Updated: 2025/04/04 20:22:24 by rerodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,38 @@
 
 //MANIPULATIONS -----------------------------------------------------
 
-double	dot_product(t_tuple *a, t_tuple *b)
+double	dot_product(t_tuple a, t_tuple b)
 {
 	return (
-		(a->x * b->x) + \
-		(a->y * b->y) + \
-		(a->z * b->z) + \
-		(a->w * b->w)
+		(a.x * b.x) + \
+		(a.y * b.y) + \
+		(a.z * b.z) + \
+		(a.w * b.w)
 	);
 }
 
-double	magnitude(t_tuple *t)
+double	magnitude(t_tuple t)
 {
 	return (sqrt(dot_product(t, t)));
 }
 
-t_tuple	normalize(t_tuple *t)
+t_tuple	normalize(t_tuple t)
 {
 	const double	_magnitude = magnitude(t);
 
 	return (create_tuple(
-			(t->x / _magnitude),
-			(t->y / _magnitude),
-			(t->z / _magnitude),
-			(t->w / _magnitude))
+			(t.x / _magnitude),
+			(t.y / _magnitude),
+			(t.z / _magnitude),
+			(t.w / _magnitude))
 	);
 }
 
-t_tuple	cross_product(t_tuple *a, t_tuple *b)
+t_tuple	cross_product(t_tuple a, t_tuple b)
 {
 	return (create_vector(
-			((a->y * b->z) - (a->z * b->y)),
-			((a->z * b->x) - (a->x * b->z)),
-			((a->x * b->y) - (a->y * b->x)))
+			((a.y * b.z) - (a.z * b.y)),
+			((a.z * b.x) - (a.x * b.z)),
+			((a.x * b.y) - (a.y * b.x)))
 	);
 }
