d68b9342940821c85631d4ef918e22a2a5a05456
Joao Francisco Perpectuo Figueiredo <jperpect@c2r6s2.42porto.com>
2025-04-30 15:35:54 +0100

    remuve the files

--- src/loop/mause.c
+++ src/loop/mause.c
@@ -6,7 +6,7 @@
 /*   By: jperpct <jperpect@student.42porto.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 15:13:28 by jperpct           #+#    #+#             */
-/*   Updated: 2025/04/30 15:20:49 by jperpct          ###   ########.fr       */
+/*   Updated: 2025/04/30 15:34:14 by jperpct          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,5 @@ void mouse(t_minirt *rt_struct)
 	ok = lig_view_transform(rt_struct->point,scalar_mult_tuples(rat.direction, 1), create_vector(0, 1,0));
 
         rt_struct->cam_m = cm_init( WALL_X ,WALL_Y , M_PI/2, ok);
-
-	
 
 }
