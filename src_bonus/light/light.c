927f1c974b19b9e5a8e0a59ad4b87b771321d01c
Joao Francisco Perpectuo Figueiredo <jperpect@c1r14s2.42porto.com>
2025-06-18 10:43:54 +0100

    ok

--- src_bonus/light/light.c
+++ src_bonus/light/light.c
@@ -37,8 +37,12 @@ t_color	lig_reflect_color(t_minirt *rt_struct, t_computations comp)
 	obj = comp.object;
 	i++;
 	point = add_tuples(comp.point, scalar_mult_tuples(comp.norm, EPSILON));
-	if (comp.object == NULL || obj == NULL || obj->matiral.reflect == 0)
+	if (comp.object == NULL || obj == NULL || obj->matiral.reflect
+		== 0 || i == 16)
+	{
+		i = 0;
 		return (c_new(0, 0, 0));
+	}
 	reflec = ray_gener(point, comp.reflect);
 	color = lig_color_at(rt_struct, reflec);
 	return (c_scalar_multipl(color, obj->matiral.reflect));
